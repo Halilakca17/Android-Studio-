be772fa5a5e703441b925f971018ff6b735a800e
Halil İbrahim Akça <akcahalil969@gmail.com>
2023-05-24 13:46:39 +0300

    Catch The Kenny game

---
+++ CatchTheKenny/app/src/main/java/com/example/catchthekenny/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.catchthekenny;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView scoreText;
+    TextView timeText;
+    int score;
+    ImageView imageView, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9;
+    ImageView[] imageViewsArray;
+    Handler handler;
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeText = (TextView) findViewById(R.id.timeText);
+        scoreText = (TextView) findViewById(R.id.scoreText);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+        int[] imageViewIds = {R.id.imageView, R.id.imageView2, R.id.imageView3, R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8, R.id.imageView9};
+        imageViewsArray = new ImageView[imageViewIds.length];
+        for (int i = 0; i < imageViewIds.length; i++) {
+            imageViewsArray[i] = findViewById(imageViewIds[i]);
+        }
+        hideImages();
+
+        score = 0;
+        new CountDownTimer(12000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeText.setText("Time: " + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                timeText.setText("Time off!");
+                handler.removeCallbacks(runnable);
+                for (ImageView image : imageViewsArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Restart ?");
+                alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Game over!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.show();
+            }
+        }.start();
+    }
+
+    public void increaseScore(View view) {
+        score++;
+        scoreText.setText("Score: " + score);
+    }
+
+    public void hideImages() {
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (ImageView image : imageViewsArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random random = new Random();
+                int i = random.nextInt(9);
+                imageViewsArray[i].setVisibility(View.VISIBLE);
+                handler.postDelayed(this, 450);
+            }
+        };
+        handler.post(runnable);
+    }
+}
